6e887ed0fe11b7f659649bf1ad6fe94c42fbf6a3
hz <hz>
2023-07-20 16:51:10 +0600

    first commit

--- news/views.py
+++ news/views.py
@@ -1,7 +1,48 @@
-from django.shortcuts import render
-from .models import Artiles
+from django.shortcuts import render, redirect
+from .models import Articles
+from .forms import ArticlesForm
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 
 def news_home(reguest):
-    news = Artiles.objects.all()
+    news = Articles.objects.order_by('-date')
     return render(reguest, 'news/news_home.html', {'news': news})
+
+
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'Articles'
+
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+    template_name = 'news/create.html'
+
+    form_class = ArticlesForm
+
+
+class NewsDeleteView(DeleteView):
+    model = Articles
+    success_url = '/news/'
+    template_name = 'news/news-delete.html'
+
+
+def create(reguest):
+    error = ''
+    if reguest.method == 'POST':
+        form = ArticlesForm(reguest.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была неверной'
+
+    form = ArticlesForm()
+
+    data = {
+        'form': form,
+        'error': error
+    }
+
+    return render(reguest, 'news/create.html', data)
